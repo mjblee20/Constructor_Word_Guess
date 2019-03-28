0c2bbe2ec686263ec45dfcdf13c820d978a78ac7
Monte Cristo <mjblee20@D-10-18-214-145.dhcp4.washington.edu>
2019-03-28 13:47:57 -0700

    initialize files and begin pseudo code

---
+++ word.js
@@ -0,0 +1,8 @@
+// create an object representing the current word the user is attempting to guess
+
+
+// An array of new Letter objects representing the letters of the underlying word
+
+// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+
+// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
